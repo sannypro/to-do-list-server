803ffacac0cc44d0166b487846b1002c666f0b26
sannypro <sannymr79@gmail.com>
2022-05-18 22:16:42 +0600

    environment varriables done

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 
-// b1mLrJ6GrClB2Lwf
+// 
 
 app.use(cors())
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://to-do-list:b1mLrJ6GrClB2Lwf@cluster0.ifs5i.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://to-do-list:${process.env.PASS_MONGODB}@cluster0.ifs5i.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     await client.connect();
@@ -25,7 +25,7 @@ async function run() {
         app.get("/note", async (req, res) => {
             const email = req.query.email
             const query = { email: email }
-            console.log(query);
+
             const result = await notesCollection.find(query).toArray();
             res.send(result)
 
